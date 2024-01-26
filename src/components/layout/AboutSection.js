fe2786c73d1c8747331053fba804de9c7789969a
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-26 18:06:05 -0500

    add about section commponent

---
+++ src/components/layout/AboutSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function AboutSection() {
+  return (
+    <div className='max-w-2xl mx-auto mt-4 text-gray-500 flex flex-col gap-4'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam officiis,
+        aliquam reiciendis fugit laborum illum nulla voluptatem quod, quidem
+        molestias ad! Architecto tempora blanditiis alias nesciunt ullam,
+        pariatur omnis officiis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim placeat
+        error consectetur non asperiores est sit laborum vel fuga, impedit
+        cupiditate minus iure natus laudantium assumenda quae aperiam amet
+        porro!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim placeat
+        error consectetur non asperiores est sit laborum vel fuga.
+      </p>
+    </div>
+  );
+}
