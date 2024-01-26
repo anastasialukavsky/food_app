d1a0c7cbc411a307723452153cd6aff7c2a72892
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-26 18:05:37 -0500

    modularize home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,31 @@
 import { Header, Hero } from '@/components/layout';
+import AboutSection from '@/components/layout/AboutSection';
+import ContactSection from '@/components/layout/ContactSection';
+import Footer from '@/components/layout/Footer';
 import HomeMenu from '@/components/layout/HomeMenu';
+import SectionHeaders from '@/components/layout/SectionHeaders';
 import React from 'react';
-
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <HomeMenu/>
+      <HomeMenu />
+
+      <section className='text-center mt-20'>
+        <SectionHeaders subheader={'our Story'} mainHeader={'About Us'} />
+        <AboutSection />
+      </section>
+
+      <section className='text-center mt-20'>
+        <SectionHeaders
+          subheader={"don't hesitate"}
+          mainHeader={'Contact Us'}
+        />
+        <ContactSection />
+      </section>
+      <Footer />
     </>
   );
 }
