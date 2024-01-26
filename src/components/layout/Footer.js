a591bd1eba5d8bf0d55b4181a68947c1a9427b07
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-26 18:06:23 -0500

    add footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='border-t p-8 text-gray-500 mt-16'>&copy; 2024 All right reserved</footer>
+  );
+}
