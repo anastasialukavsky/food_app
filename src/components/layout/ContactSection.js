c8da30828e497bd2f9741849a15400b01d1b4186
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-26 18:06:16 -0500

    add contact section component

---
+++ src/components/layout/ContactSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactSection() {
+  return (
+    <div className='my-3 text-lg text-gray-600 underline '>
+      <a href='tel:+1 (347) 543 1186'>+1 (347) 543 1186</a>
+    </div>
+  );
+}
