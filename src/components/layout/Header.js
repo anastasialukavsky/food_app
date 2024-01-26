b26ff9d7bdb2b83202b9d34cf1d5d2f2da30f8c8
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-26 18:06:46 -0500

    add signup to navbar, restructure nav menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-export  function Header() {
+export function Header() {
   return (
     <header className='flex items-center justify-between'>
-      <Link href='' className='text-primary font-semibold text-4xl '>
-        PIZZA PLACE
-      </Link>
       <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
+        <Link href='' className='text-primary font-semibold text-4xl '>
+          PIZZA PLACE
+        </Link>
         <Link href={''}>Home</Link>
         <Link href={''}>Menu</Link>
         <Link href={''}>About</Link>
         <Link href={''}>Contact</Link>
+      </nav>
+      <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
         <Link
-          href={''}
+          href={'/login'}
           className='bg-primary rounded-full text-white px-8 py-2'
         >
           Login
         </Link>
+        <Link href={'/signup'}>Sign Up</Link>
       </nav>
     </header>
   );
