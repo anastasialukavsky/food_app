923f75170324a077568ff316a0ff555a8a1cb45f
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-29 22:26:41 -0500

    add user  schema

---
+++ src/app/models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model, models } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate: (password) => {
+        if (!password?.length || password.length < 5) {
+          throw new Error('password must be at least 5 characters');
+        }
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = models?.User || model('User', UserSchema);
