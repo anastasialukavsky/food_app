f8f5d7ecb533911abe414abcb6f68cbaff089c9d
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-29 22:27:13 -0500

    render children out of main

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
 import { Roboto } from 'next/font/google';
 import './globals.css';
+// import { Header, Footer } from '@/components/layout';
+// import { Header } from '@/components/layout';
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['100', '400', '500', '700'] });
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '400', '500', '700'],
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={roboto.className}>
-      <main className='max-w-4xl  mx-auto p-4'>{children}</main>
+        <main className='max-w-4xl  mx-auto p-4'>
+          {children}
+        </main>
       </body>
     </html>
   );
