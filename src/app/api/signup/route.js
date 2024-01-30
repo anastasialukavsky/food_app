9e8a860c0ac6bffa3a83ae6f9424c83aee69f58c
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-29 22:25:58 -0500

    add post rt handler

---
+++ src/app/api/signup/route.js
@@ -0,0 +1,10 @@
+import { User } from './../../models/User';
+import mongoose from 'mongoose';
+// import User from '@/app/models/User'
+
+export async function POST(req) {
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createUser = await User.create(body);
+  return Response.json(createUser);
+}
