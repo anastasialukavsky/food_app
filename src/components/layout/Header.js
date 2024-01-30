d6db8e605d7bb19912773315c3f56aa8fd6db8c4
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-29 22:28:09 -0500

    add default export and root route paths

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 
-export function Header() {
+export default function Header() {
   return (
     <header className='flex items-center justify-between'>
       <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
-        <Link href='' className='text-primary font-semibold text-4xl '>
+        <Link href={'/'} className='text-primary font-semibold text-4xl '>
           PIZZA PLACE
         </Link>
-        <Link href={''}>Home</Link>
+        <Link href={'/'}>Home</Link>
         <Link href={''}>Menu</Link>
         <Link href={''}>About</Link>
         <Link href={''}>Contact</Link>
